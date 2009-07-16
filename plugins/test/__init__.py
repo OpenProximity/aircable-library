2ffe6acfd3ba2d8b8b548da6224b1eced70a0cb0
naranjo.manuel <none@none>
2009-07-16 19:36:38 +0000

    BIG Not Important change added license to all files!.
    new file:   django-web/pluginsystem.py
    new file:   op_lib/plugins/test/__init__.py
    new file:   op_lib/plugins/test/templates/test/hello.html
    new file:   op_lib/plugins/test/urls.py
    new file:   op_lib/plugins/test/views.py
    Added working plugin system. Sample added.
    
    modified:   remoteScanner/pair.py
    modified:   remoteScanner/remotescanner.py
    modified:   remoteScanner/test-client.py
    modified:   server.sh
    Added rssi calibration methods
    
    --HG--
    rename : openproximity2/django-web/__init__.py => openproximity2/op_lib/plugins/__init__.py
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%4045

--- plugins/test/__init__.py
+++ plugins/test/__init__.py
@@ -14,36 +14,16 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-BASE_SIGNAL = 100
-MAX_SIGNAL = BASE_SIGNAL + 200
+# A test plugin
 
-DONGLES_ADDED=BASE_SIGNAL 
-NO_DONGLES=BASE_SIGNAL+1
-
-CYCLE_COMPLETE=BASE_SIGNAL+20
-CYCLE_START=BASE_SIGNAL+21
-CYCLE_SCAN_DONGLE=BASE_SIGNAL+22
-
-FOUND_DEVICE=BASE_SIGNAL+30
-
-TEXT = {
-	DONGLES_ADDED:		'Dongles added',
-	NO_DONGLES: 		'No dongles to add',
-	
-	CYCLE_COMPLETE: 	'Scan cycle completed',
-	CYCLE_START:		'Scan cycle start',
-	CYCLE_SCAN_DONGLE:	'Scan cycle on dongle',
-	
-	FOUND_DEVICE:		'Found device',
-}
-
-POST = {
-	DONGLES_ADDED:		'scanner_dongle_added',
-	NO_DONGLES: 		'scanner_no_dongle',
-	
-	CYCLE_COMPLETE: 	'scanner_cycle_complete',
-	CYCLE_START:		'scanner_cycle_start',
-	CYCLE_SCAN_DONGLE:	'scanner_cycle_scan_start',
-	
-	FOUND_DEVICE:		'scanner_cycle_found_device',
+provides = { 
+    'name': 'sample plugin', 		# friendly name
+    
+    'django': True,			# expose me as a django enabled plugin
+    
+    'TEMPLATE_DIRS': 'templates',	# static media I give to django
+    'LOCALE_PATHS': 'locale',
+    'INSTALLED_APPS': None,
+    
+    'urls': ( 'test', 'urls' )		# urls I give to django
 }
