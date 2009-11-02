86046cc7a1ac4980004957e85e300984855069c7
naranjo.manuel <none@none>
2009-11-02 12:44:11 +0000

    Moved some files to our library renamed:    serverXR/const.py -> op_lib/net/aircable/const.py renamed:    serverXR/procworker.py -> op_lib/net/aircable/procworker.py renamed:    serverXR/sdp.py -> op_lib/net/aircable/sdp.py renamed:    serverXR/utils.py -> op_lib/net/aircable/utils.py renamed:    serverXR/wrappers.py -> op_lib/net/aircable/wrappers.py Make all this library classes so plugins can use them
    modified:   serverXR/manager.py
    modified:   serverXR/scanner.py
    modified:   serverXR/uploader.py
    Use libraries.
    
    deleted:    serverXR/workqueue.py
    No more needed
    
    --HG--
    rename : openproximity2/serverXR/const.py => openproximity2/op_lib/net/aircable/const.py
    rename : openproximity2/serverXR/procworker.py => openproximity2/op_lib/net/aircable/procworker.py
    rename : openproximity2/serverXR/sdp.py => openproximity2/op_lib/net/aircable/sdp.py
    rename : openproximity2/serverXR/utils.py => openproximity2/op_lib/net/aircable/utils.py
    rename : openproximity2/serverXR/wrappers.py => openproximity2/op_lib/net/aircable/wrappers.py
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40127

---
+++ net/aircable/sdp.py
@@ -0,0 +1,67 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#!/usr/bin/python
+
+import sys
+import dbus
+import re
+
+from utils import logger
+
+from lxml import etree
+
+CHANNEL_XPATH=etree.XPath("/record/attribute[@id='0x0004']/sequence/sequence/uuid[@value='0x0003']/../uint8/@value")
+
+OBEX_UUID='00001105-0000-1000-8000-00805f9b34fb'
+FTP_UUID='00001106-0000-1000-8000-00805f9b34fb'
+
+							
+def resolve(address, uuid, adapter=None, bus=dbus.SystemBus()):
+	try:
+	    adapter.CreateDevice(address)
+	    logger.debug("Device created: %s" % address)
+	except:
+	    logger.debug("Device all ready known: %s" % address)
+	    
+	path = adapter.FindDevice(address)
+	
+	device = dbus.Interface(bus.get_object("org.bluez", path),
+							"org.bluez.Device")
+	properties = device.GetProperties()
+	
+	if uuid in properties['UUIDs']:
+	    services = device.DiscoverServices(uuid);
+		
+	    for key in services.keys():
+		root=etree.XML(str(services[key]))
+		res=CHANNEL_XPATH(root)
+		if len(res)>0:
+		    return int(res[0], 16)
+	
+	# lets check if the service is there now
+	#adapter.RemoveDevice(path)
+	
+	raise Exception("UUID not found")
+		
+if __name__ == '__main__':
+    bus = dbus.SystemBus()
+    manager = dbus.Interface(bus.get_object("org.bluez", "/"), "org.bluez.Manager")
+
+    adapter = dbus.Interface(bus.get_object("org.bluez", manager.DefaultAdapter()),
+							"org.bluez.Adapter")
+
+    print resolve('00:15:E9:F5:BA:FE', OBEX_UUID, adapter, bus)
+    print resolve('00:15:E9:F5:BA:FE', FTP_UUID, adapter, bus)
---
+++ net/aircable/wrappers.py
@@ -0,0 +1,47 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+import dbus
+import const
+import utils
+
+class Adapter(object):
+	'''A wrapper arround an adapter'''
+	dbus_path = None
+	dbus_interface = None
+	bt_address = None
+	is_aircable = False
+	bus = None
+
+	def __init__(self, bus, path, name="OpenProximity 2.0"):
+		self.dbus_path = path
+		self.dbus_interface = dbus.Interface( bus.get_object(const.BLUEZ, path), const.BLUEZ_ADAPTER )
+		#self.bt_address = self.dbus_interface.GetAddress()
+		self.bt_address=str(self.dbus_interface.GetProperties()['Address'])
+		self.dbus_interface.SetProperty('Powered', True)
+		self.is_aircable = utils.isAIRcable(self.bt_address)
+		self.bus = bus
+		self.dbus_interface.SetProperty('Name', name)
+		utils.logger.debug("Initializated Adapter(%s, %s)" % (self, self.is_aircable)) 
+
+	def __str__(self):
+		return '%s, %s' % (self.bt_address, self.dbus_path)
+
+if __name__=='__main__':
+	import dbus.glib
+	import gobject
+	adapter = Adapter( dbus.SystemBus(),'/org/bluez/hci0')
+	print adapter, adapter.aircable
+ 
